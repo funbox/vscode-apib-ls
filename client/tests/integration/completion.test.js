3401870d2ac17284775aa16f912d5e527b7d2234
mdnsk <m.donsk@ya.ru>
2021-06-17 17:17:23 +0300

    Add tests for HTTP methods autocompletion

--- client/tests/integration/completion.test.js
+++ client/tests/integration/completion.test.js
@@ -12,6 +12,7 @@ describe('Extension test', function () {
   const fixturePath = path.resolve(__dirname, '../tmp-fixtures');
   const fixtureSourcePath = path.resolve(__dirname, '../fixtures');
 
+  const incompleteMethodsUri = helpers.getDocUri('incomplete-methods.apib');
   const incompleteSectionNamesUri = helpers.getDocUri('incomplete-section-names.apib');
 
   before(() => {
@@ -28,43 +29,65 @@ describe('Extension test', function () {
     });
 
     it('Completes "Data Structures" keyword', async () => {
-      await testCompletion(incompleteSectionNamesUri, new vscode.Position(2, 5), {
-        items: [
-          { label: 'Data Structures', kind: vscode.CompletionItemKind.Keyword },
-        ],
-      });
+      await testCompletion(incompleteSectionNamesUri, new vscode.Position(2, 5), ['Data Structures']);
     });
 
     it('Completes "Schema Structures" keyword', async () => {
-      await testCompletion(incompleteSectionNamesUri, new vscode.Position(3, 4), {
-        items: [
-          { label: 'Schema Structures', kind: vscode.CompletionItemKind.Keyword },
-        ],
-      });
+      await testCompletion(incompleteSectionNamesUri, new vscode.Position(3, 4), ['Schema Structures']);
     });
 
     it('Completes "Resource Prototypes" keyword', async () => {
-      await testCompletion(incompleteSectionNamesUri, new vscode.Position(4, 4), {
-        items: [
-          { label: 'Resource Prototypes', kind: vscode.CompletionItemKind.Keyword },
-        ],
-      });
+      await testCompletion(incompleteSectionNamesUri, new vscode.Position(4, 4), ['Resource Prototypes']);
     });
 
     it('Completes "Group" keyword', async () => {
-      await testCompletion(incompleteSectionNamesUri, new vscode.Position(5, 5), {
-        items: [
-          { label: 'Group', kind: vscode.CompletionItemKind.Keyword },
-        ],
-      });
+      await testCompletion(incompleteSectionNamesUri, new vscode.Position(5, 5), ['Group']);
     });
 
     it('Completes "Import" keyword', async () => {
-      await testCompletion(incompleteSectionNamesUri, new vscode.Position(6, 4), {
-        items: [
-          { label: 'Import', kind: vscode.CompletionItemKind.Keyword },
-        ],
-      });
+      await testCompletion(incompleteSectionNamesUri, new vscode.Position(6, 4), ['Import']);
+    });
+  });
+
+  describe('Completes request methods', () => {
+    before(async () => {
+      await helpers.activate(incompleteSectionNamesUri);
+    });
+
+    it('Completes resource methods', async () => {
+      const cases = [
+        [new vscode.Position(2, 5), ['GET']],
+        [new vscode.Position(3, 4), ['PATCH', 'POST', 'PROPPATCH', 'PUT']],
+        [new vscode.Position(4, 6), ['DELETE']],
+        [new vscode.Position(5, 4), ['OPTIONS']],
+        [new vscode.Position(6, 4), ['LINK', 'LOCK']],
+        [new vscode.Position(7, 4), ['UNLINK', 'UNLOCK']],
+        [new vscode.Position(8, 4), ['CONNECT', 'COPY']],
+        [new vscode.Position(9, 4), ['MKCOL', 'MOVE']],
+        [new vscode.Position(10, 4), ['HEAD']],
+      ];
+
+      await Promise.all(cases.map(async ([position, labels]) => (
+        testCompletion(incompleteMethodsUri, position, labels)
+      )));
+    });
+
+    it('Completes named resource methods ', async () => {
+      const cases = [
+        [new vscode.Position(14, 11), ['GET']],
+        [new vscode.Position(15, 11), ['PATCH', 'POST', 'PROPPATCH', 'PUT']],
+        [new vscode.Position(16, 11), ['DELETE']],
+        [new vscode.Position(17, 11), ['OPTIONS']],
+        [new vscode.Position(18, 11), ['LINK', 'LOCK']],
+        [new vscode.Position(19, 11), ['UNLINK', 'UNLOCK']],
+        [new vscode.Position(20, 11), ['CONNECT', 'COPY']],
+        [new vscode.Position(21, 11), ['MKCOL', 'MOVE']],
+        [new vscode.Position(22, 11), ['HEAD']],
+      ];
+
+      await Promise.all(cases.map(async ([position, labels]) => (
+        testCompletion(incompleteMethodsUri, position, labels)
+      )));
     });
   });
 });
@@ -72,21 +95,21 @@ describe('Extension test', function () {
 /**
  * @param {vscode.Uri} docUri
  * @param {vscode.Position} position
- * @param {vscode.CompletionList} expectedCompletionList
+ * @param {string[]} label
  * @returns {Promise<void>}
  */
-async function testCompletion(docUri, position, expectedCompletionList) {
+async function testCompletion(docUri, position, labels) {
   const actualCompletionList = (await vscode.commands.executeCommand(
     'vscode.executeCompletionItemProvider',
     docUri,
     position,
   ));
 
-  assert.ok(actualCompletionList.items.length === expectedCompletionList.items.length);
+  assert.ok(actualCompletionList.items.length === labels.length);
 
-  expectedCompletionList.items.forEach((expectedItem, i) => {
+  labels.forEach((label, i) => {
     const actualItem = actualCompletionList.items[i];
-    assert.strictEqual(actualItem.label, expectedItem.label);
-    assert.strictEqual(actualItem.kind, expectedItem.kind);
+    assert.strictEqual(actualItem.label, label);
+    assert.strictEqual(actualItem.kind, vscode.CompletionItemKind.Keyword);
   });
 }
