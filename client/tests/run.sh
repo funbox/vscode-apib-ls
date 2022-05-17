ad056bfb5baeb7f5cc34626156625ea65e537bfd
igoradamenko <mail@igoradamenko.com>
2022-05-17 15:27:42 +0300

    Install deps on CI

--- client/tests/run.sh
+++ client/tests/run.sh
@@ -4,8 +4,8 @@ set -e
 set -o xtrace
 
 if [[ -n "${CI}" ]]; then
-  apt-get update
-  apt-get install -y xvfb libsecret-1-0 iproute2
+  sudo apt-get update
+  sudo apt-get install -y xvfb libsecret-1-0 iproute2
   Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
   XVFB_PID=$!
   export DISPLAY=":99"
